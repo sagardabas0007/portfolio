296a3f7ffb0bd8c5e0a2c390ddf739f8447bc664
sagar <sagardabas2019@gmail.com>
2025-08-28 18:08:21 +0530

    added animation

--- src/Components/header/Navbar.jsx
+++ src/Components/header/Navbar.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import My from '../../assets/my.jpg'; 
+import React from "react";
+import My from "../../assets/my.jpg";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-    const btns = ["about", "me", "get started"];
+  const btns = ["about", "me", "get started"];
 
   return (
-      <div className='flex items-center justify-between px-5 py-2'>
-          <div className='flex items-center justify-between gap-2'>
-              <img src={My} alt="broken image" className='h-8 w-7 rounded-full' />
-              <p className='text-sm font-medium'>Sagar Dabas</p>
-          </div>
-      <div className='flex items-center justify-between gap-2'>
-        <a href="#" className='font-extralight text-sm hover:text-gray-500 transition-all duration-100'>Hi@dabas.dev</a>
-       {btns.map((label, index) => (
-         < button 
-            key={index} 
-            className='bg-white text-sm font-normal px-4 py-[2px] rounded-md text-[#1c1b1b] hover:bg-gray-100 transition-all duration-150 shadow-[0_0_10px_rgba(0,0,0,0.9) cursor-pointer'
+    <motion.div
+      initial={{ opacity: 0,y:-100 }}
+      animate={{ opacity: 1, y:0 }}
+      transition={{
+        type:"spring",
+        stiffness: 200,
+        damping:90,
+        delay: 1,
+        duration: 0.3,
+      }}
+      className="flex items-center justify-between px-5 py-2"
+    >
+      <div className="flex items-center justify-between gap-2">
+        <img src={My} alt="broken image" className="h-8 w-7 rounded-full" />
+        <p className="text-sm font-medium">Sagar Dabas</p>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <a
+          href="#"
+          className=" hidden md:block font-extralight text-sm hover:text-gray-500 transition-all duration-100"
+        >
+          Hi@dabas.dev
+        </a>
+        {btns.map((label, index) => (
+          <button
+            key={index}
+            className="bg-white text-sm font-normal px-4 py-[2px] rounded-md text-[#1c1b1b] hover:bg-gray-100 transition-all duration-150 shadow-[0_0_10px_rgba(0,0,0,0.9) cursor-pointer"
           >
             {label}
           </button>
         ))}
-          </div>
-      
-    </div>
-  )
-}
+      </div>
+    </motion.div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,47 +1,142 @@
-import React from 'react'
-import { FaInstagram, FaGithub } from 'react-icons/fa'
+import React from "react";
+import { FaInstagram, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const text = "I'm a developer crafting thoughtful digital solutions";
+  const words = text.split(" "); // split into words
+
+  // Parent container with stagger
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2, // stagger each word by 0.2s
+      },
+    },
+  };
+
+  // Individual word animation
+  const wordAnimation = {
+    hidden: { y: 50, opacity: 0, scale: 1.7 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      scale: 1,
+      transition: { type: "spring", stiffness: 90, damping: 12 },
+    },
+  };
+
   return (
-    <div className='bg-white h-auto w-auto mx-5 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-lg flex flex-col justify-center items-center gap-y-7 pt-28'>
-      <div className='flex items-center justify-between gap-2'>
-        <div className='bg-teal-400 h-2 w-2 rounded-full animate-pulse'></div>
-        <div className='font-light text-sm'>Available for new projects</div>
-      </div>
+    <div className="bg-white h-auto w-auto mx-5 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-lg flex flex-col justify-center items-center gap-y-7 pt-28">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping:90,
+          delay: 2,
+          duration: 0.3,
+        }}
+        className="flex items-center justify-between gap-2"
+      >
+        <div className="bg-teal-400 h-2 w-2 rounded-full animate-pulse"></div>
+        <div className="font-light text-sm">Available for new projects</div>
+      </motion.div>
 
-      <div className='text-center text-7xl font-[550]'>
-        I'm a developer crafting<br/> thoughtful digital solutions
-      </div>
-
-      <div className='text-center font-light text-sm'>
-        I blend strategic thinking with intuitive design to create user<br/>-centered digital journeys that deliver results.
-      </div>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        className="text-center text-6xl md:text-7xl lg:text-8xl font-[550] flex flex-wrap justify-center gap-x-3"
+      >
+        {words.map((word, index) => (
+          <motion.span
+            key={index}
+            variants={wordAnimation}
+            className="inline-block"
+          >
+            {word}
+          </motion.span>
+        ))}
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 2,
+          duration: 0.3,
+        }}
+        className="text-center font-light text-sm"
+      >
+        I blend strategic thinking with intuitive design to create user
+        <br />
+        -centered digital journeys that deliver results.
+      </motion.div>
 
       {/* hire button and socials */}
 
-      <div className='flex justify-between items-center gap-3'>
-        <a href="#"><button className='bg-[#FAD6A5] px-4 py-2 rounded-lg text-base font-medium shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer hover:shadow-[0_0_10px_rgba(0,0,0,0.4)] transition-all duration-700'>let's connect</button></a>
-        <a href="https://www.instagram.com/dabas.sagar_0007/"><FaInstagram className='h-5 w-5'/></a>
-        <a href="https://x.com/sagar_dbs?s=21"><FaXTwitter className='h-5 w-5'/></a>
-        <a href="https://github.com/sagardabas0007"><FaGithub className='h-5 w-5'/></a>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 2,
+          duration: 0.3,
+        }}
+        className="flex justify-between items-center gap-3"
+      >
+        <a href="#">
+          <button className="bg-[#FAD6A5] px-4 py-2 rounded-lg text-base font-medium shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer hover:shadow-[0_0_10px_rgba(0,0,0,0.4)] transition-all duration-700">
+            let's connect
+          </button>
+        </a>
+        <a href="https://www.instagram.com/dabas.sagar_0007/">
+          <FaInstagram className="h-5 w-5" />
+        </a>
+        <a href="https://x.com/sagar_dbs?s=21">
+          <FaXTwitter className="h-5 w-5" />
+        </a>
+        <a href="https://github.com/sagardabas0007">
+          <FaGithub className="h-5 w-5" />
+        </a>
+      </motion.div>
 
-      <div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 2,
+          duration: 0.3,
+        }}
+      >
         <hr class=" border-gray-300 w-[600px] mt-7" />
-      </div>
+      </motion.div>
 
-      <div className=''>
-        <div className='text-center font-light text-sm'>
-          "Sagar transformed our digital presence with his<br/> thoughtful, results-driven approach to design."
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 2,
+          duration: 0.3,
+        }}
+        className=""
+      >
+        <div className="text-center font-light text-sm">
+          "Sagar transformed our digital presence with his
+          <br /> thoughtful, results-driven approach to design."
         </div>
-        <div className='flex items-baseline justify-end gap-2 text-green-700 animate-bounce'>
-          <span>-</span><a href="https://www.ref3r.com/" className='pt-4 text-md font-medium'>ref3r.com</a>
+        <div className="flex items-baseline justify-end gap-2 text-green-700 animate-bounce">
+          <span>-</span>
+          <a href="https://www.ref3r.com/" className="pt-4 text-md font-medium">
+            ref3r.com
+          </a>
         </div>
-      </div>
-
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
